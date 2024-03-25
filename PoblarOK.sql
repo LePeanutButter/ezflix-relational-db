3c27562938f79ab519f328ab09be3b21fecc3704
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-03-24 19:18:46 -0500

    Update PoblarOK.sql

--- PoblarOK.sql
+++ PoblarOK.sql
@@ -1,3 +1,9 @@
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (1, 'Boris', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 1000, 1000, '1:52:02', 'Bartell, Kiehn and Rath');
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (2, 'Elmo', TO_DATE('24/03/2022', 'DD/MM/YYYY'), 'Hintz, Watsic', 2000, 2000, '1:52:02', 'Lindgren-Spinka');
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (3, 'Ava', TO_DATE('24/03/2021', 'DD/MM/YYYY'), 'Schmidt, Kreiger', 3000, 3000, '1:52:02', 'Bergnaum-Schimmel');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (100000000000000000, 'Boris', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 1000, 1000, '1:52:02', 'Bartell, Kiehn and Rath');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (200000000000000000, 'Elmo', TO_DATE('24/03/2022', 'DD/MM/YYYY'), 'Hintz, Watsic', 2000, 2000, '1:52:02', 'Lindgren-Spinka');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (300000000000000000, 'Ava', TO_DATE('24/03/2021', 'DD/MM/YYYY'), 'Schmidt, Kreiger', 3000, 3000, '1:52:02', 'Bergnaum-Schimmel');
+
+INSERT INTO Series (id,  director, fecha, productora, precioCompra, precioRenta, distribuidor) VALUES (300000000000000001, 'Wolfgang', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Hoeger, Gerhold and Quitzon', 1000, 1000, 'Gibson and Sons');
+INSERT INTO Series (id,  director, fecha, productora, precioCompra, precioRenta, distribuidor) VALUES (300000000000000002, 'Anya', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Pfannerstill LLC', 2000, 2000, 'Quitzon, Stamm and Greenfelder');
+INSERT INTO Series (id,  director, fecha, productora, precioCompra, precioRenta, distribuidor) VALUES (300000000000000003, 'Dena', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Macejkovic and Sons', 3000, 3000, 'Boyle Group');
+
+
