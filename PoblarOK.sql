b5a0fa35e78965f3bc975d044c5bbb04988b00e6
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-03-24 19:11:21 -0500

    Update PoblarOK.sql

--- PoblarOK.sql
+++ PoblarOK.sql
@@ -1,3 +1,3 @@
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (1, 'Boris', '7/21/2023', 'Kertzmann-Kub', 1000, 1000, '1:52:02', 'Bartell, Kiehn and Rath');
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (2, 'Elmo', '12/29/2023', 'Hintz, Watsica and O''Keefe', 2000, 2000, '1:52:02', 'Lindgren-Spinka');
-INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (3, 'Ava', '6/11/2023', 'Schmidt, Kreiger and Stoltenberg', 3000, 3000, '1:52:02', 'Bergnaum-Schimmel');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (1, 'Boris', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 1000, 1000, '1:52:02', 'Bartell, Kiehn and Rath');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (2, 'Elmo', TO_DATE('24/03/2022', 'DD/MM/YYYY'), 'Hintz, Watsic', 2000, 2000, '1:52:02', 'Lindgren-Spinka');
+INSERT INTO Peliculas (id,  director, fecha, productora, precioCompra, precioRenta, duracion, distriubuidor) VALUES (3, 'Ava', TO_DATE('24/03/2021', 'DD/MM/YYYY'), 'Schmidt, Kreiger', 3000, 3000, '1:52:02', 'Bergnaum-Schimmel');
