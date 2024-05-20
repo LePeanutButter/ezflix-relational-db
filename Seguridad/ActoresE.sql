2b66dee46a4afefef5edc9dbbe6711edda659dc8
LePeanutButter <157855016+LePeanutButter@users.noreply.github.com>
2024-05-19 19:30:33 -0500

    Seguridad

--- Seguridad/ActoresE.sql
+++ Seguridad/ActoresE.sql
@@ -1,14 +1,37 @@
 CREATE OR REPLACE PACKAGE PA_Cliente AS
-    PROCEDURE adicionarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contraseña IN VARCHAR2, p_telefono IN NUMBER);
-    PROCEDURE adicionarMetodosDePago();
-    PROCEDURE adicionarBibliotecas();
-    PROCEDURE adicionarOperacion();
-    FUNCTION consultarPeliculas();
-    FUNCTION consultarSeries();
+    PROCEDURE adicionarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contrasena IN VARCHAR2, p_telefono IN NUMBER);
+    PROCEDURE adicionarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE);
+    PROCEDURE adicionarBibliotecas(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2);
+    PROCEDURE adicionarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2);
+    PROCEDURE consultarPeliculas;
+    PROCEDURE consultarSeries;
 END PA_Cliente;
 /
 
-CREATE OR REPLACE PACKAGE BODY PA_Gerente AS
+CREATE OR REPLACE PACKAGE PA_Gerente AS
     PROCEDURE consultarBase(p_tabla IN VARCHAR2);
 END PA_Gerente;
+/
+
+CREATE OR REPLACE PACKAGE PA_Auditor AS
+    PROCEDURE consultarAuditoria;
+END PA_Auditor;
+/
+
+CREATE OR REPLACE PACKAGE PA_Soporte AS
+    PROCEDURE adicionarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contrasena IN VARCHAR2, p_telefono IN NUMBER);
+    PROCEDURE consultarCuenta;
+    PROCEDURE modificarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contrasena IN VARCHAR2, p_telefono IN NUMBER);
+    PROCEDURE eliminarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2);
+    PROCEDURE adicionarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE);
+    PROCEDURE consultarMetodosDePago;
+    PROCEDURE modificarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE);
+    PROCEDURE eliminarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER);
+    PROCEDURE adicionarBiblioteca(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2);
+    PROCEDURE consultarBiblioteca;
+    PROCEDURE modificarBiblioteca(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2, biblioteca IN VARCHAR2);
+    PROCEDURE eliminarBiblioteca(nombreCuenta IN VARCHAR2, p_nombre IN NUMBER);
+    PROCEDURE adicionarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2);
+    PROCEDURE eliminarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2);
+END PA_Soporte;
 /
--- Seguridad/ActoresI.sql
+++ Seguridad/ActoresI.sql
@@ -3,9 +3,13 @@ CREATE OR REPLACE PACKAGE BODY PA_Cliente AS
     BEGIN
         IF p_nombre IS NOT NULL AND p_correo IS NOT NULL AND p_contrasena IS NOT NULL AND p_telefono IS NOT NULL THEN
             INSERT INTO Cuentas (nombre, correo, contrasena, telefono) VALUES (p_nombre, p_correo, p_contrasena, p_telefono);
+            COMMIT;
         ELSE
             RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
         END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
     END adicionarCuenta;
     
     PROCEDURE adicionarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE) AS
@@ -15,9 +19,13 @@ CREATE OR REPLACE PACKAGE BODY PA_Cliente AS
         
         IF p_idCuenta IS NOT NULL AND p_numero IS NOT NULL AND p_nombre IS NOT NULL AND p_cvv IS NOT NULL AND p_fechaExpiracion IS NOT NULL THEN
             INSERT INTO MetodosDePago (idCuenta, numero, nombre, cvv, fechaExpiracion) VALUES (p_idCuenta, p_numero, p_nombre, p_cvv, p_fechaExpiracion);
+            COMMIT;
         ELSE
             RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
         END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
     END adicionarMetodosDePago;
 
     PROCEDURE adicionarBibliotecas(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2) AS
@@ -27,9 +35,13 @@ CREATE OR REPLACE PACKAGE BODY PA_Cliente AS
         
         IF p_idCuenta IS NOT NULL AND p_nombre IS NOT NULL THEN
             INSERT INTO Bibliotecas (idCuenta, nombre) VALUES (p_idCuenta, p_nombre);
+            COMMIT;
         ELSE
             RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
         END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
     END adicionarBibliotecas;
 
     PROCEDURE adicionarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2) AS
@@ -59,20 +71,39 @@ CREATE OR REPLACE PACKAGE BODY PA_Cliente AS
             ELSE
                 RAISE_APPLICATION_ERROR(-20002, 'Tipo de operación no válido.');
             END IF;
+            COMMIT;
         ELSE
             RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
         END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
     END adicionarOperacion;
 
     PROCEDURE consultarPeliculas AS
-        BEGIN
-            SELECT * FROM Peliculas;
-        END consultarPeliculas;
+        v_id Peliculas.id%TYPE;
+        v_director Peliculas.director%TYPE;
+        v_fechaLanzamiento Peliculas.fechaLanzamiento%TYPE;
+        v_productora Peliculas.productora%TYPE;
+        v_precioCompra Peliculas.precioCompra%TYPE;
+        v_precioRenta Peliculas.precioRenta%TYPE;
+        v_duracion Peliculas.duracion%TYPE;
+        v_distribuidor Peliculas.distribuidor%TYPE;
+    BEGIN
+        SELECT id, director, fechaLanzamiento, productora, precioCompra, precioRenta, duracion, distribuidor INTO v_id, v_director, v_fechaLanzamiento, v_productora, v_precioCompra, v_precioRenta, v_duracion, v_distribuidor FROM Peliculas;
+    END consultarPeliculas;
 
-    PROCEDURE consultarSeries
-        BEGIN
-            SELECT * FROM Series;
-        END consultarSeries;
+    PROCEDURE consultarSeries AS
+        v_id Peliculas.id%TYPE;
+        v_director Peliculas.director%TYPE;
+        v_fechaLanzamiento Peliculas.fechaLanzamiento%TYPE;
+        v_productora Peliculas.productora%TYPE;
+        v_precioCompra Peliculas.precioCompra%TYPE;
+        v_precioRenta Peliculas.precioRenta%TYPE;
+        v_distribuidor Peliculas.distribuidor%TYPE;
+    BEGIN
+        SELECT id, director, fechaLanzamiento, productora, precioCompra, precioRenta, distribuidor INTO v_id, v_director, v_fechaLanzamiento, v_productora, v_precioCompra, v_precioRenta, v_distribuidor FROM Series;
+    END consultarSeries;
 END PA_Cliente;
 /
 
@@ -88,8 +119,221 @@ END PA_Gerente;
 
 CREATE OR REPLACE PACKAGE BODY PA_Auditor AS
     PROCEDURE consultarAuditoria AS
+        v_idOperacion Auditorias.idOperacion%TYPE;
+        v_operacion Auditorias.operacion%TYPE;
+        v_fechaOperacion Auditorias.fechaOperacion%TYPE;
     BEGIN
-        SELECT * FROM Auditorias;
+        SELECT idOperacion, operacion, fechaOperacion INTO v_idOperacion, v_operacion, v_fechaOperacion FROM Auditorias;
     END consultarAuditoria;
 END PA_Auditor;
+/
+
+CREATE OR REPLACE PACKAGE BODY PA_Soporte AS
+    PROCEDURE adicionarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contrasena IN VARCHAR2, p_telefono IN NUMBER) AS
+    BEGIN
+        IF p_nombre IS NOT NULL AND p_correo IS NOT NULL AND p_contrasena IS NOT NULL AND p_telefono IS NOT NULL THEN
+            INSERT INTO Cuentas (nombre, correo, contrasena, telefono) VALUES (p_nombre, p_correo, p_contrasena, p_telefono);
+            COMMIT;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
+        END IF;
+        EXCEPTION
+            WHEN OTHERS THEN
+                ROLLBACK;
+    END adicionarCuenta;
+
+    PROCEDURE consultarCuenta AS
+    v_id Cuentas.id%TYPE;
+    v_nombre Cuentas.nombre%TYPE;
+    v_correo Cuentas.correo%TYPE;
+    v_contrasena Cuentas.contrasena%TYPE;
+    v_telefono Cuentas.telefono%TYPE;
+    v_fechaCreacion Cuentas.fechaCreacion%TYPE;
+    BEGIN
+        SELECT id, nombre, correo, contrasena, telefono, fechaCreacion INTO v_id, v_nombre, v_correo, v_contrasena, v_telefono, v_fechaCreacion FROM Cuentas;
+    END consultarCuenta;
+
+    PROCEDURE modificarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2, p_contrasena IN VARCHAR2, p_telefono IN NUMBER) AS
+        FUNCTION cifrado(input VARCHAR2) RETURN VARCHAR2 IS
+            output VARCHAR2(20);
+        BEGIN
+            output := SUBSTR(input, 6, 1) || SUBSTR(input, 1, 5) || SUBSTR(input, 11, 1)|| SUBSTR(input, 7, 4) || SUBSTR(input, 16, 5) || SUBSTR(input, 12, 3) || SUBSTR(input, 20, 1) || SUBSTR(input, 8, 3) || SUBSTR(input, 2, 4) || SUBSTR(input, 19, 1) || SUBSTR(input, 15, 1) || SUBSTR(input, 13, 1) || SUBSTR(input, 3, 4) || SUBSTR(input, 17, 2) || SUBSTR(input, 18, 1) || SUBSTR(input, 10, 2) || SUBSTR(input, 14, 1) || SUBSTR(input, 4, 4) || SUBSTR(input, 9, 1);
+            RETURN output;
+        END cifrado;
+    BEGIN
+        IF p_nombre IS NOT NULL AND p_correo IS NOT NULL THEN
+            IF contrasena IS NOT NULL THEN
+                UPDATE Cuentas SET contrasena = cifrado(p_contrasena) WHERE nombre = p_nombre AND correo = p_correo;
+            ELSIF p_telefono IS NOT NULL THEN
+                UPDATE Cuentas SET telefono = p_telefono WHERE nombre = p_nombre AND correo = p_correo;
+            ELSIF p_contrasena IS NOT NULL AND p_telefono IS NOT NULL THEN
+                UPDATE Cuentas SET contrasena = cifrado(p_contrasena), telefono = p_telefono WHERE nombre = p_nombre AND correo = p_correo;
+            END IF;
+            COMMIT;
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END modificarCuenta;
+
+    PROCEDURE eliminarCuenta(p_nombre IN VARCHAR2, p_correo IN VARCHAR2) AS
+    BEGIN
+        DELETE FROM Cuentas WHERE nombre = p_nombre AND correo = p_correo;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END eliminarCuenta;
+
+    PROCEDURE adicionarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        IF p_idCuenta IS NOT NULL AND p_numero IS NOT NULL AND p_nombre IS NOT NULL AND p_cvv IS NOT NULL AND p_fechaExpiracion IS NOT NULL THEN
+            INSERT INTO MetodosDePago (idCuenta, numero, nombre, cvv, fechaExpiracion) VALUES (p_idCuenta, p_numero, p_nombre, p_cvv, p_fechaExpiracion);
+            COMMIT;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END adicionarMetodosDePago;
+
+    PROCEDURE consultarMetodosDePago AS
+    BEGIN
+        SELECT * FROM MetodosDePago;
+    END consultarMetodosDePago;
+
+    PROCEDURE modificarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER, p_nombre IN VARCHAR2, p_cvv IN NUMBER, p_fechaExpiracion IN DATE) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        IF p_idCuenta IS NOT NULL AND p_numero IS NOT NULL AND (p_nombre IS NOT NULL OR p_cvv IS NOT NULL OR p_fechaExpiracion IS NOT NULL) THEN
+            UPDATE MetodosDePago SET nombre = COALESCE(p_nombre, nombre), cvv = COALESCE(p_cvv, cvv), fechaExpiracion = COALESCE(p_fechaExpiracion, fechaExpiracion) WHERE idCuenta = p_idCuenta AND numero = p_numero;
+            COMMIT;
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END modificarMetodosDePago;
+
+    PROCEDURE eliminarMetodosDePago(nombreCuenta IN VARCHAR2, p_numero IN NUMBER) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        DELETE FROM MetodosDePago WHERE idCuenta = p_idCuenta AND numero = p_numero;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END eliminarMetodosDePago;
+
+    PROCEDURE adicionarBiblioteca(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        
+        IF p_idCuenta IS NOT NULL AND p_nombre IS NOT NULL THEN
+            INSERT INTO Bibliotecas (idCuenta, nombre) VALUES (p_idCuenta, p_nombre);
+            COMMIT;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END adicionarBiblioteca;
+
+    PROCEDURE consultarBiblioteca AS
+    BEGIN
+        SELECT * FROM Bibliotecas;
+    END consultarBiblioteca;
+
+    PROCEDURE modificarBiblioteca(p_nombre IN VARCHAR2, nombreCuenta IN VARCHAR2, biblioteca IN VARCHAR2) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        IF p_nombre IS NOT NULL AND p_idCuenta IS NOT NULL THEN
+            UPDATE Bibliotecas SET nombre = p_nombre WHERE idCuenta = p_idCuenta AND nombre = biblioteca;
+        END IF;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END modificarBiblioteca;
+
+    PROCEDURE eliminarBiblioteca(nombreCuenta IN VARCHAR2, p_nombre IN NUMBER) AS
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        DELETE FROM Bibliotecas WHERE idCuenta = p_idCuenta AND nombre = p_nombre;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END eliminarBiblioteca;
+
+    PROCEDURE adicionarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2) AS
+        p_idPelicula Versiones.idPelicula%TYPE;
+        p_idSerie Versiones.idSerie%TYPE;
+        p_idCuenta Cuentas.id%TYPE;
+        p_idOperacion Operaciones.id%TYPE;
+    BEGIN
+        SELECT idPelicula, idSerie INTO p_idPelicula, p_idSerie FROM Versiones WHERE nombreContenido = nombreContenido;
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        
+        IF p_idPelicula IS NOT NULL AND p_idCuenta IS NOT NULL THEN
+            INSERT INTO Operaciones(idCuenta) VALUES (p_idCuenta) RETURNING id INTO p_idOperacion;
+            
+            IF tipoOperacion = 'Compra' THEN
+                IF p_idPelicula IS NOT NULL THEN
+                    INSERT INTO Compras(idOperacion, idPelicula) VALUES (p_idOperacion, p_idPelicula);
+                ELSE
+                    INSERT INTO Compras(idOperacion, idSerie) VALUES (p_idOperacion, p_idSerie);
+                END IF;
+            ELSIF tipoOperacion = 'Renta' THEN
+                IF p_idPelicula IS NOT NULL THEN
+                    INSERT INTO Rentas(idOperacion, idPelicula) VALUES (p_idOperacion, p_idPelicula);
+                ELSE
+                    INSERT INTO Rentas(idOperacion, idSerie) VALUES (p_idOperacion, p_idSerie);
+                END IF;
+            ELSE
+                RAISE_APPLICATION_ERROR(-20002, 'Tipo de operación no válido.');
+            END IF;
+            COMMIT;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001, 'Los datos no son del tipo esperado.');
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+    END adicionarOperacion;
+
+    PROCEDURE eliminarOperacion(nombreCuenta IN VARCHAR2, nombreContenido IN VARCHAR2, tipoOperacion IN VARCHAR2) AS
+        p_idPelicula Versiones.idPelicula%TYPE;
+        p_idSerie Versiones.idSerie%TYPE;
+        p_idCuenta Cuentas.id%TYPE;
+    BEGIN
+        SELECT idPelicula, idSerie INTO p_idPelicula, p_idSerie FROM Versiones WHERE nombreContenido = nombreContenido;
+        SELECT id INTO p_idCuenta FROM Cuentas WHERE nombre = nombreCuenta;
+        IF tipoOperacion = "Compra" THEN
+            IF p_idPelicula IS NOT NULL THEN
+                DELETE FROM Operaciones WHERE idCuenta = p_idCuenta AND id = (SELECT idOperacion FROM Compras JOIN Operaciones ON (Compras.idOperacion = Operaciones.id) WHERE Operaciones.idCuenta = p_idCuenta AND Compras.idPelicula = p_idPelicula);
+            ELSIF p_idSerie IS NOT NULL THEN
+                DELETE FROM Operaciones WHERE idCuenta = p_idCuenta AND id = (SELECT idOperacion FROM Compras JOIN Operaciones ON (Compras.idOperacion = Operaciones.id) WHERE Operaciones.idCuenta = p_idCuenta AND Compras.idSerie = p_idSerie);
+            END IF;
+        ELSIF tipoOperacion = "Renta" THEN
+            IF p_idPelicula IS NOT NULL THEN
+                DELETE FROM Operaciones WHERE idCuenta = p_idCuenta AND id = (SELECT idOperacion FROM Rentas JOIN Operaciones ON (Rentas.idOperacion = Operaciones.id) WHERE Operaciones.idCuenta = p_idCuenta AND Rentas.idPelicula = p_idPelicula);
+            ELSIF p_idSerie IS NOT NULL THEN
+                DELETE FROM Operaciones WHERE idCuenta = p_idCuenta AND id = (SELECT idOperacion FROM Rentas JOIN Operaciones ON (Rentas.idOperacion = Operaciones.id) WHERE Operaciones.idCuenta = p_idCuenta AND Rentas.idSerie = p_idSerie);
+            END IF;
+        END IF;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;    
+    END eliminarOperacion;
+END PA_Soporte;
 /
