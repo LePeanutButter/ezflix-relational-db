eae21e39f6efd0c4fdb40a40e63d8f197ae83a84
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-05-22 14:02:57 -0500

    Update CRUDOK.sql

--- CRUDOK.sql
+++ CRUDOK.sql
@@ -17,15 +17,20 @@ BEGIN
     PC_Distribuidores.Adicionar('Warner');
 END;
 
+BEGIN
+    PC_Distribuidores.eliminar('Warner');
+END;
+
+
 SELECT * FROM distribuidores;
 
 --PC_Peliculas
 BEGIN
-    PC_Peliculas.Adicionar('Boris', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 10000, 10000, '01:52:02','Fox');
+    PC_Peliculas.Adicionar('Boris', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 10000, 10000, '01:52:02','Warner');
 END;
 
 BEGIN
-    PC_Peliculas.Modificar('Martin', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 10000, 10000, '01:52:02','Fox','P8613412119617787861');
+    PC_Peliculas.Modificar('Martin', TO_DATE('24/03/2023', 'DD/MM/YYYY'), 'Kertzmann-Kub', 10000, 10000, '01:52:02','Warner','P8613412119617787861');
 END;
 SELECT * FROM peliculas;
 
@@ -42,17 +47,31 @@ SELECT * FROM series;
 
 --PC_Biblioteca
 BEGIN
-    PC_Bibliotecas.Adicionar('C5686158024051638299','El cartero');
+    PC_Bibliotecas.Adicionar('C3817645174719816590','El cartero');
 END;
 
 SELECT * FROM bibliotecas;
 
 BEGIN
-    PC_Bibliotecas.Modificar('B2573031438117822608','Sin nombre');
+    PC_Bibliotecas.Modificar('B1139185203606516298','Sin nombre');
 END;
 
 
 --PC_Compras
+INSERT INTO operaciones (idCuenta) VALUES ('C3683029930598508348');
+SELECT * FROM operaciones;
+
+BEGIN
+    PC_Compras.Adicionar('O1555928058360617425','P4679343241730333767',NULL,3000);
+END;
+
+SELECT * FROM compras;
+
+BEGIN
+    PC_Compras.Modificar('O1555928058360617425',400000);
+END;
+
+SELECT * FROM compras;
 
 
 
