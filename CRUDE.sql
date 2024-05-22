3b740c891a977631cff896880e46e1f47b458b72
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-05-22 10:25:15 -0500

    Update CRUDE.sql

--- CRUDE.sql
+++ CRUDE.sql
@@ -72,3 +72,64 @@ END PC_Cuentas;
     END consultar;
 END PC_Cuentas;
 /
+
+--CRUDE
+CREATE OR REPLACE PACKAGE PC_Distribuidores AS
+    PROCEDURE Adicionar(
+        nombre IN VARCHAR2
+    );
+    PROCEDURE Modificar(
+        nombre IN VARCHAR2,
+        id IN VARCHAR2
+    );
+    PROCEDURE Eliminar(
+        nombre IN VARCHAR2,
+        id IN VARCHAR2
+    );
+    PROCEDURE Consultar(
+        nombre IN VARCHAR2,
+        resultado OUT SYS_REFCURSOR
+    );
+END PC_Distribuidores;
+/
+--CRUDI
+CREATE OR REPLACE PACKAGE BODY PC_Distribuidores AS
+    PROCEDURE Adicionar(
+        nombre IN VARCHAR2
+    )IS
+    BEGIN
+        INSERT INTO Distribuidores (nombre)
+        VALUES (nombre);
+    END Adicionar;
+    
+    PROCEDURE Modificar(
+        nombre IN VARCHAR2,
+        id IN VARCHAR2
+    ) IS
+    BEGIN
+        UPDATE Distribuidores 
+        SET nombre = nombre
+        WHERE id = id;
+    END Modificar;
+    
+    PROCEDURE Eliminar(
+        nombre IN VARCHAR2,
+        id IN VARCHAR2
+    ) IS
+    BEGIN
+        DELETE FROM Distribuidores WHERE id = id;
+    END Eliminar;
+    
+    PROCEDURE Consultar(
+        nombre IN VARCHAR2,
+        resultado OUT SYS_REFCURSOR
+    ) IS
+    BEGIN 
+        OPEN resultado FOR
+        SELECT * FROM distribuidores WHERE nombre = nombre;
+    END Consultar;
+    
+END PC_distribuidores;
+/
+    
+    
