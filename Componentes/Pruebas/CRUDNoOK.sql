22f81fde15136909967d447e6c0bff575808f8c0
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-05-26 18:54:20 -0500

    Create CRUDNoOK.sql

---
+++ Componentes/Pruebas/CRUDNoOK.sql
@@ -0,0 +1,14 @@
+--PC_Cuentas
+BEGIN
+    PC_Cuentas.Adicionar('Miguel Paramo','megusta@disqus.com','superman');
+    
+END;
+
+BEGIN 
+    PC_Cuentas.Eliminar('Miguel Paramo');
+END;
+
+--PC_Biblioteca
+BEGIN
+    PC_Bibliotecas.Adicionar('C3817645174719816590','El cartero','FECHA');
+END;
